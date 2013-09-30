b66c3b98ecde0577d5dbcb38477e99c3a50fd662
elcodedocle <gael.abadin@gmail.com>
2013-09-30 22:31:54 +0200

    Update Weighted.java

--- org/grouplens/mooc/cbf/dao/Weighted.java
+++ org/grouplens/mooc/cbf/dao/Weighted.java
@@ -1,11 +1,9 @@
 package org.grouplens.mooc.cbf.dao;
 
 /**
- * Created with IntelliJ IDEA.
- * User: Mess
+ * User: Gael Abadin
  * Date: 30/09/13
  * Time: 9:37
- * To change this template use File | Settings | File Templates.
  */
 public class Weighted {
     public final boolean flag;
